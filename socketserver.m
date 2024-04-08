84d639c6b3cd1dc98b111d6cbebb3d9f982247be
Wereowen <owenchanchaktao@gmail.com>
2024-04-07 21:20:56 -0400

    corrected (de)serialization

--- socketserver.m
+++ socketserver.m
@@ -12,8 +12,8 @@ function server(port)
         t = datetime('now');
         val = sin(2 * pi * (second(t)));
         dataToSend = [0, 0, 0, 0, 0, val, 0];
-        % wrong serialization
-        serializedData = typecast(dataToSend, 'uint8');
+        
+        serializedData = serialize(dataToSend);
 
         if server.Connected
             write(server, serializedData)
@@ -25,4 +25,28 @@ function server(port)
 
     clear global stopServer;
     delete(server);
+end
+
+function dataBytes = serialize(data)
+    % Check endianness
+    if ~isequal(typecast(swapbytes(uint16(1)), 'uint8'), [1, 0])
+        % The system is little-endian, swap the byte order of the data array
+        dataBytes = typecast(swapbytes(data), 'uint8');
+    else
+        % The system is big-endian, no need to swap bytes
+        dataBytes = typecast(data, 'uint8');
+    end
+end
+
+function data = deserialize(dataBytes)
+    dataUint64 = typecast(dataBytes, 'uint64');
+
+    % Check endianness
+    if ~isequal(typecast(swapbytes(uint16(1)), 'uint8'), [1, 0])
+        % The system is little-endian, swap the byte order
+        dataUint64 = swapbytes(dataUint64);
+    end
+
+    % Convert the uint64 data back to doubles
+    data = typecast(dataUint64, 'double');
 end
